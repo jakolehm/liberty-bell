0670fa5a1c1b2c55bc03ec7ef1422fe42b88554a
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2018-07-22 09:53:38 +0300

    initial gitlab specs

---
+++ spec/commands/gitlab/authenticate_spec.rb
@@ -0,0 +1,33 @@
+RSpec.describe Gitlab::Authenticate do
+  let(:valid_data) do
+    {
+      apiVersion: TokenReview::API_VERSION,
+      kind: TokenReview.name,
+      spec: {
+        token: '123'
+      }
+    }
+  end
+
+  describe '#execute' do
+    it 'returns authenticated on valid token' do
+      stub_request(:get, "https://gitlab.com/api/v4/user").to_return( body: { username: 'johndoe' }.to_json )
+      stub_request(:get, "https://gitlab.com/api/v4/groups").to_return( body: '[]' )
+
+      subject = described_class.new(valid_data)
+      outcome = subject.run
+      expect(outcome.success?).to be_truthy
+      expect(outcome.result.status.authenticated).to be_truthy
+      expect(outcome.result.status.user.username).to eq('johndoe')
+    end
+
+    it 'returns not authenticated on invalid token' do
+      stub_request(:get, "https://gitlab.com/api/v4/user").to_return( status: 403 )
+
+      subject = described_class.new(valid_data)
+      outcome = subject.run
+      expect(outcome.success?).to be_truthy
+      expect(outcome.result.status.authenticated).to be_falsey
+    end
+  end
+end
